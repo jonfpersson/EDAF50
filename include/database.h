9fb1679ab49e069521700d7aa59821283abcb398
David Larsson <davidelarsson70@outlook.com>
2025-04-01 12:27:15 +0200

    Modified databse header

--- include/database.h
+++ include/database.h
@@ -6,12 +6,20 @@
 #include <iostream>
 #include "newsgroup.h"
 
-class Database {
+class Database
+{
 public:
-    virtual ~Database() {}  
-    
-    virtual void addNewsGroup(const Newsgroup&) = 0;  
-    virtual void addArticle(const Article&, const Newsgroup&) = 0;  
+    virtual ~Database() {}
+
+    virtual void addNewsGroup(const Newsgroup &) = 0;
+    virtual bool removeNewsGroup(const std::string &groupId) = 0;
+    virtual std::vector<Newsgroup &> getNewsGroups() = 0; 
+
+    virtual void addArticle(const Article &, const std::string &groupId) = 0;
+    virtual Article &getArticle(const std::string &groupId, const std::string &articleId) = 0;
+    virtual std::vector<Article &> getArticles(const std::string &groupId) = 0;
+    virtual bool &deleteArticle(const std::string &groupId, const std::string &articleId) = 0;
+
 };
 
 #endif
