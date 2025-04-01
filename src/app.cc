c227129445049f1bd0457183bf3c87b2778a48ef
j0nfp <jonfpersson@gmail.com>
2025-04-01 11:31:01 +0200

    stuff

--- src/app.cc
+++ src/app.cc
@@ -1,8 +1,29 @@
+#include "../include/diskdb.h"
+#include "../include/article.h"
+#include "../include/database.h"
+#include <iostream>
 
-#include "connection.h"
+int main() {
+    // Create a Newsgroup
+    Newsgroup ng("Tech News", "001");
 
-int main(int argc, char *argv[])
-{
+    // Create an Article
+    Article art("AI Advances", "John Doe", "AI is changing the world!", 101);
+
+    // Print Newsgroup details
+    std::cout << "Newsgroup Created: " << ng.getName() << " (ID: " << ng.getId() << ")\n";
+
+    // Print Article details
+    std::cout << "Article Created:\n";
+    std::cout << "Title: " << art.getTitle() << "\n";
+    std::cout << "Author: " << art.getAuthor() << "\n";
+    std::cout << "Text: " << art.getText() << "\n";
+    std::cout << "ID: " << art.getId() << "\n";
+
+    // Create Database object and add the newsgroup/article
+    Diskdb db;
+    db.addNewsGroup(ng);
+    db.addArticle(art, ng);
 
     return 0;
 }
--- src/diskdb.cc
+++ src/diskdb.cc
@@ -1,14 +1,39 @@
 #include "diskdb.h"
 #include <filesystem>
+#include <fstream>
 
-void Diskdb::addNewsGroup(const std::string& name){
-    std::filesystem::create_directory(name);
+void Diskdb::addNewsGroup(const Newsgroup& newsgroup){
+    std::string dir_name = newsgroup.getName() + " - " + newsgroup.getId();
+   /* if(std::filesystem::exists(dir_name)){
+        std::cout << "Newsgroup already exists!" << std::endl;
+        return;
+    }*/
+
+    std::filesystem::create_directory(dir_name);
 
 }
 
 void Diskdb::addArticle(const Article& article, const Newsgroup& newsgroup){
-    std::cout << "addArticle" << std::endl;
-    if(std::filesystem::exists(article.getTitle())){
+
+    //format for a newgroup (directories)
+    // name - id
+    std::string dir_name = newsgroup.getName() + " - " + newsgroup.getId();
+    std::string file_name = article.getTitle() + " - " + std::to_string(article.getId());
+
+    if(std::filesystem::exists(dir_name + "/" + file_name)){
         std::cout << "Article already exists!" << std::endl;
+        return;
     }
+    std::ofstream file(file_name);
+
+    if (file.is_open()) {
+        file << article.getAuthor() << std::endl;
+        file << article.getText() << std::endl;
+    
+        file.close();
+        //cout << "File created successfully." << endl;
+    } else {
+        std::cout << "Error in creating file!" << std::endl;
+    }
+
 }
